9ad91c810ea2f12df80868fcd7e80d6919f97273
Imarius <onspccc@163.com>
2026-05-31 18:41:58 +0800

    Configure Quartz v4 and set baseUrl

--- quartz.config.ts
+++ quartz.config.ts
@@ -15,7 +15,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "ima-rius.github.io/my-quartz-blog",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
